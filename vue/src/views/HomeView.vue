6361bd1595f001c97ded5915123b182012830a35
FreeReol <2571219329@qq.com>
2023-03-03 14:47:15 +0800

    前端初步搭建

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,12 +1,35 @@
 <template>
   <div>
-    <el-button type="primary">按钮</el-button>
+    <!-- 搜索表单 -->
+    <div>
+      <el-input style="width: 240px;" placeholder="请输入名称"></el-input>
+      <el-button type="primary" style="margin-left: 5px;"><i class="el-icon-search"></i><span>搜索</span></el-button>
+    </div>
+    <el-table :data="tableData" stripe>
+      <el-table-column prop="name" label="名称"></el-table-column>
+      <el-table-column prop="age" label="年龄"></el-table-column>
+      <el-table-column prop="address" label="地址"></el-table-column>
+      <el-table-column prop="phone" label="联系方式"></el-table-column>
+    </el-table>
+    <!-- 分页 -->
+    <el-pagination style="margin-top: 20px;" background layout="prev,paper,next"
+      :total="tableData.length"></el-pagination>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HomeView',
+  data() {
+    return {
+      tableData: [
+        { name: '王二', age: 20, address: '北京市', phone: '13899008899' },
+        { name: '王二', age: 20, address: '北京市', phone: '13899008899' },
+        { name: '王二', age: 20, address: '北京市', phone: '13899008899' },
+        { name: '王二', age: 20, address: '北京市', phone: '13899008899' },
+      ]
+    }
+  }
 
 }
 </script>
